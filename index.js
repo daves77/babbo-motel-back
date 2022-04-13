2821a413971d3eae712231046112a73895b6630e
daves <davidlimjh77@gmail.com>
2022-04-13 15:06:25 +0800

    test deploy

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-const dotenv = require('dotenv')
-const cookieParser =require('cookie-parser')
-const cors = require('cors')
-const http = require("http")
-const express = require('express')
-const socketio = require('socket.io')
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
 
-const initSockets = require('./sockets')
-const bindRoutes = require('./routers')
-const { init } = require('express/lib/application')
+const initSockets = require('./sockets');
+const bindRoutes = require('./routers');
+const { init } = require('express/lib/application');
 
-dotenv.config()
-const PORT = process.env.PORT || '3004'
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000"
+dotenv.config();
+const PORT = process.env.PORT || '3004';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+console.log(FRONTEND_URL);
+const app = express();
 
-
-const app = express()
-
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = socketio(server, {
-  cors: {
-    origin: FRONTEND_URL,
-    methods: ["GET", "POST"]
-  }
-})
+	cors: {
+		origin: FRONTEND_URL,
+		methods: ['GET', 'POST'],
+	},
+});
 
-initSockets(io)
+initSockets(io);
 
-app.use(cors({
-  credentials: true,
-  origin: FRONTEND_URL
-}))
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
-app.use(express.json({limit: "2mb"}))
-app.use(express.static('public'))
+app.use(cors());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '2mb' }));
+app.use(express.static('public'));
 
-bindRoutes(app)
+bindRoutes(app);
 
-server.listen(PORT, () => console.log(`listening @ ${PORT}`))
+app.get('/test', (req, res) => res.json({ msg: 'hellowrld' }));
 
+server.listen(PORT, () => console.log(`listening @ ${PORT}`));
