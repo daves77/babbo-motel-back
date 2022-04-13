898da11e653cc2b985a669df358912cc10ccfdb8
daves <davidlimjh77@gmail.com>
2022-04-13 17:07:30 +0800

    removed changes

--- index.js
+++ index.js
@@ -16,16 +16,7 @@ console.log(FRONTEND_URL);
 const app = express();
 
 app.use(cors(
-  {credentials: true}
 ));
-
-
-app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-  next()
-})
 
 const server = http.createServer(app);
 const io = socketio(server, {
