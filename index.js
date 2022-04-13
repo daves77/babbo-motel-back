4605c1639a53ab592b4a6967c9702900203622f5
daves <davidlimjh77@gmail.com>
2022-04-13 16:56:22 +0800

    trying so hard to fix cors policy

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(cors(
   {credentials: true}
 ));
 
+
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next()
+})
+
 const server = http.createServer(app);
 const io = socketio(server, {
 	cors: {
